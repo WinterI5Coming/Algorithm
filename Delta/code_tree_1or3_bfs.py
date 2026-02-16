550f04b56702b25297fd8dbb56a532dd4ca41e2e
WinterI5Coming <dutjddnr1224@naver.com>
2026-02-16 13:45:59 +0900

    solve: 코드트리 7일차 과제 bfs, dfs 두 ë가지 해결

--- Delta/code_tree_1or3_bfs.py
+++ Delta/code_tree_1or3_bfs.py
@@ -4,29 +4,28 @@ n = int(input())
 status = list(map(int, input().split()))
 status.reverse()
 
+# 큐를 이용하여 시도 bfs [(현재 idx, 이동 횟수)]
+# 1칸, 3칸의 두 가지 선택지가 존재한다.
+
 visited = [False] * n
 q = deque()
 
-# 첫번째 칸 부터 진행한다
-# 모든 경우의 수를 stack으로 관리
-# [(현재 위치, 이동 횟수)]
 q.append((0, 0))
 visited[0] = True
 
-answer = -1
+ans = -1
 
 while q:
     idx, cnt = q.popleft()
 
     if idx == n - 1:
-        answer = cnt
+        ans = cnt
         break
 
     for step in (1, 3):
-        ni = idx + step
+        n_idx = idx + step
+        if n_idx < n and status[n_idx] != 0 and not visited[n_idx]:
+            visited[n_idx] = True
+            q.append((n_idx, cnt + 1))
 
-        if 0 <= ni < n and status[ni] != 0 and not visited[ni]:
-            visited[ni] = True
-            q.append((ni, cnt + 1))
-
-print(answer)
+print(ans)
--- Delta/code_tree_1or3_dfs.py
+++ Delta/code_tree_1or3_dfs.py
@@ -6,33 +6,38 @@ status.reverse()
 
 # 첫번째 칸 부터 진행한다
 # 모든 경우의 수를 stack으로 관리
-# [(현재 위치, 이동 횟수, 성공 여부)]
+# [(현재 위치, 이동 횟수)]
 stack = [(0, 0)]
-INF = 10**18
+INF = 10 ** 19
 dist = [INF] * n
-min_cnt = n + 1
+min_cnt = INF
 
 while stack:
-    idx, m_cnt = stack.pop()
+    idx, cnt = stack.pop()
 
-    if m_cnt > min_cnt:
+    # 가지치기
+    # dist에 등록된 같은 idx에 왔는데 더 많은 횟수로 온 경우
+    # 예를 들어, 3번째 칸을 한 번에 갈 수 있는 반면, 1칸씩 세번으로 갈 수도 있다
+    # 이런 비효율적인 경우는 제외해줘야 한다
+    # 1칸을 먼저 하고 3칸을 나중에 하기 때문에
+    if cnt > dist[idx]:
         continue
 
+    # idx가 끝이면 이동 완료
+    # => min_cnt 갱신
     if idx == n - 1:
+        min_cnt = min(min_cnt, cnt)
         continue
 
-    if idx != n - 1:
-        # 현재 idx 에서 이동 경우의 수 판단
-        # (idx + 1) idx로 이동
-        # (idx + 3) idx로 이동
-        if idx + 1 < n and status[idx + 1] != 0:
-            new_m_cnt = m_cnt + 1
-            dist[idx + 1] = new_m_cnt
-            stack.append((idx + 1, new_m_cnt))
+    # 현재 idx 에서 이동 경우의 수 판단
+    # (idx + 1) idx로 이동
+    # (idx + 3) idx로 이동
+    for step in (1, 3):
+        n_idx = idx + step
+        if n_idx < n and status[n_idx] != 0:
+            new_cnt = cnt + 1
+            if new_cnt < dist[n_idx]:
+                dist[n_idx] = new_cnt
+                stack.append((n_idx, new_cnt))
 
-        if idx + 3 < n and status[idx + 3] != 0:
-            new_m_cnt = m_cnt + 1
-            dist[idx + 3] = new_m_cnt
-            stack.append((idx + 3, new_m_cnt))
-
-print(dist[-1] if dist[-1] != INF else -1)
+print(min_cnt if min_cnt != INF else -1)
