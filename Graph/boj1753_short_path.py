d2211444084debc18986d2c7d95dbad9cd43c066
WinterI5Coming <dutjddnr1224@naver.com>
2026-04-06 14:13:13 +0900

    solve: boj1753_최단경로 해결. (모든 간선의 가중치가 자연수임과 동시에 모두 다르기 때문에 다익스트라를 적용해서 풀어야 하는 전형적인 문제였다. 방문 배열을 dist라는 거리 배열을 사용해서 최단 경로를 비교하는 것으로 처리하였다. 특히 input의 입력 속도를 아래의 코드를 이용해서 최적화시킨 것이 도움이 컸다.(아래 코드 없으면 시간초과 났었다.) import sys input = sys.stdin.readline)

---
+++ Graph/boj1753_short_path.py
@@ -0,0 +1,61 @@
+"""
+방향그래프가 주어지면, 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구해야 한다.
+        단, 모든 간선의 가중치는 10이하의 자연수이다. (모든 간선의 가중치가 다르다.)
+
+=> 다익스트라
+"""
+import heapq
+import sys
+input = sys.stdin.readline
+
+
+def main():
+    V, E = map(int, input().split())
+    K = int(input())
+
+    graph = [[] for _ in range(V + 1)]
+    for i in range(E):
+        u, v, w = map(int, input().split())
+
+        graph[u].append((v, w))
+
+    INF = float("INF")
+    dist = [INF] * (V + 1)
+    dist[K] = 0
+
+    heap = []
+    heapq.heappush(heap, (0, K))
+
+    while heap:
+        cur_dist, cur_node = heapq.heappop(heap)
+
+        if cur_dist > dist[cur_node]:
+            continue
+
+        for nxt_node, weight in graph[cur_node]:
+            new_dist = cur_dist + weight
+
+            if new_dist < dist[nxt_node]:
+                dist[nxt_node] = new_dist
+                heapq.heappush(heap, (new_dist, nxt_node))
+
+    for i in range(1, V + 1):
+        if dist[i] == INF:
+            print("INF")
+            continue
+
+        print(dist[i])
+
+
+main()
+
+"""
+5 6
+1
+5 1 1
+1 2 6
+1 3 3
+2 3 4
+2 4 5
+3 4 6
+"""
