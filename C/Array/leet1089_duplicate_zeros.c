ef440ec8e4ba5a257c1b965ad4e4aa84438f442c
WinterI5Coming <dutjddnr1224@naver.com>
2026-04-29 21:19:27 +0900

    solve: leet1089_duplicate zeros 해결. (0이 나오면 바로 뒤에 0을 복사하면서 한 칸씩 뒤로 미루는 문제였다. 앞에서 부터 반복문을 돌리는 것이 아니라 뒤에서 부터 반복문을 돌리면서 오는 것이 포인트였다.)

---
+++ C/Array/leet1089_duplicate_zeros.c
@@ -0,0 +1,16 @@
+void duplicateZeros(int* arr, int arrSize) {
+    
+    for (int i = 0; i < arrSize; i++) {
+       
+        if (arr[i] == 0) {
+            
+            for (int j = arrSize - 1; j > i; j--) {
+                
+                arr[j] = arr[j - 1];
+            }
+            
+            i++;
+        }
+    }
+    
+}
